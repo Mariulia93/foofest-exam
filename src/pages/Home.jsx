7a2520fb89bf3a9c85fe0720a88cc5c1ef08b388
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-05-17 16:08:34 +0200

    h2 biggest festival

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,14 @@ import Nav from "../components/Nav";
 
 export default function Home() {
   return (
-    <div id="welcomeImageBackground">
-      <div className="opacityContainer">
-        <Nav />
-        <Header />
+    <>
+      <div id="welcomeImageBackground">
+        <div className="opacityContainer">
+          <Nav />
+          <Header />
+        </div>
       </div>
-    </div>
+      <h2>THE BIGGEST FESTIVAL IN EUROPE 2022</h2>
+    </>
   );
 }
