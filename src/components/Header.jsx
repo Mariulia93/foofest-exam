1966b78c516a799b80cd9942bff9ae4e0cd42d2e
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-05-17 17:48:45 +0200

    countdown color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import CountDownTimer from "@inlightmedia/react-countdown-timer";
 export default function Header() {
   return (
-    <div id="landingHeader">
-      <h1 class="logo">FOOFEST</h1>
+    <div className="landingHeader">
+      <h1 className="logo">FOOFEST</h1>
       <div id="countdownContainer">
         <CountDownTimer
           dateTime="2022-07-10T20:00:00+02:00"
           shouldShowSeparator={false}
           shouldHidePrecedingZeros
+          style={{ color: "#2F5061" }}
         ></CountDownTimer>
-        <h3> DAYS HOURS MINUTES SECONDS</h3>
+        <h3> SUNRISES HOURS MINUTES SECONDS</h3>
       </div>
       <p>BARCELONA || 10-17 JUL</p>
     </div>
