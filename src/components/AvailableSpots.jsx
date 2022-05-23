8670f074c17787941914a29e463e6fc6e3e60234
Marina Tancau <iuli0572@stud.kea.dk>
2022-05-23 22:01:13 +0200

    camping spots fetched

--- src/components/AvailableSpots.jsx
+++ src/components/AvailableSpots.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-function AvailableSpots(){
+function AvailableSpots(props){
 return(
-    <div>
-        
-    </div>
+    
+        <tr>
+            <td>{props.availableSpot.area}</td>
+            <td>{props.availableSpot.spots}</td>
+            <td>{props.availableSpot.available}</td>
+        </tr>
+    // <div>
+    //     <p>{props.area}</p>
+    //     <p>{props.spots}</p>
+    //     <p>{props.available}</p>
+    // </div>
 )
 }
 export default AvailableSpots;
