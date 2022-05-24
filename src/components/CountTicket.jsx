a9d4200a9c7f6ac51bbc068576e219f14af9b752
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-05-24 12:49:19 +0200

    disable ticket counter on 0

--- src/components/CountTicket.jsx
+++ src/components/CountTicket.jsx
@@ -1,27 +1,50 @@
 import React from "react";
 import { useState } from "react";
 
-function CountTicket(){
-    let [count, setCount] = useState(0);
+function CountTicket() {
+  let [count, setCount] = useState(0);
+  let [isDisabled, setIsDisabled] = useState(true);
 
   function incrementCount() {
-    count = count + 1;
-    setCount(count);
+    setCount((old) => old + 1);
+    setIsDisabled(false);
   }
   function decrementCount() {
-    count = count - 1;
-    setCount(count);
+    setCount((old) => old - 1);
+    if (count > 1) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
   }
-return(
+  return (
     <div className="counter">
-        <button className="plusminus" onClick={decrementCount}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash" viewBox="0 0 16 16">
-  <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
-</svg></button>
-        <div>{count}</div>
-        <button className="plusminus" onClick={incrementCount}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
-  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-</svg></button>
+      <button className="plusminus" onClick={decrementCount} disabled={isDisabled}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-dash"
+          viewBox="0 0 16 16"
+        >
+          <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+        </svg>
+      </button>
+      <div>{count}</div>
+      <button className="plusminus" onClick={incrementCount}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-plus"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+        </svg>
+      </button>
     </div>
-)
+  );
 }
 export default CountTicket;
