baad91a08bd02bd0df75b7b99a53b0a94fc8c1e3
Marina Tancau <iuli0572@stud.kea.dk>
2022-05-30 16:56:53 +0200

    delete share on Facebook

--- src/components/BandPopUp.jsx
+++ src/components/BandPopUp.jsx
@@ -26,7 +26,7 @@ export default function BandPopUp(props) {
   return (
     <div className="popUp">
       <img src={logoUrl} alt={props.band.name} />
-      <p className="logoCredits">{props.band.logoCredits}</p>
+
       <button className="closeButton" onClick={handleClose}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -39,18 +39,6 @@ export default function BandPopUp(props) {
           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
         </svg>
       </button>
-
-      <iframe
-        src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Ffoofest2022.netlify.app%2Fschedule&layout=button_count&size=small&width=96&height=20&appId"
-        width="96"
-        height="20"
-        style={({ border: "none" }, { overflow: "hidden" })}
-        scrolling="no"
-        frameborder="0"
-        allowfullscreen="true"
-        title="facebook"
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-      ></iframe>
       <div>
         <div className="popUpName">
           <h2>{props.band.name}</h2>
@@ -60,13 +48,19 @@ export default function BandPopUp(props) {
 
         <div className="bioP">
           <p>{props.band.bio.substring(0, props.band.bio.indexOf(".") + 1)}</p>
-          <p style={!readMore ? { display: "none" } : null}> {props.band.bio.substring(props.band.bio.indexOf(".") + 1)}</p>
+          <p style={!readMore ? { display: "none" } : null}>
+            {" "}
+            {props.band.bio.substring(props.band.bio.indexOf(".") + 1)}
+          </p>
           <button className="readMore" onClick={showReadMore}>
             {readMore ? "Show less" : "Read more"}
           </button>
         </div>
 
         <p>stage: {props.band.stage}</p>
+        {props.band.logoCredits && (
+          <p className="logoCredits">Image Source:{props.band.logoCredits}</p>
+        )}
       </div>
     </div>
   );
